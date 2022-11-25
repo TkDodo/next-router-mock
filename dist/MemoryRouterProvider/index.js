8f35504c7ec2bff651f2b1850af1e7a7c366d5c7
Dominik Dorfmeister <office@dorfmeister.cc>
2022-11-25 09:41:15 +0100

    add dist dir

---
+++ dist/MemoryRouterProvider/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Automatically try to export the correct version:
+module.exports = require("./next-12");
+//# sourceMappingURL=index.js.map
